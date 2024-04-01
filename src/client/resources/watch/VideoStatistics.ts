e050a769aa7516efe975ccc1adbcb18558f66b44
Benjamin Hutchings <benjamin@blhutchings.com>
2024-04-01 16:09:11 -0300

    Updated Video likeCount expression

--- src/client/resources/watch/VideoStatistics.ts
+++ src/client/resources/watch/VideoStatistics.ts
@@ -2,35 +2,47 @@ import { getNumber } from "../../util/ParserUtils.js";
 import { Map$Video } from "./Video.js";
 
 export interface Schema$VideoStatistics {
-    viewCount?: number;
-    likeCount?: number;
-    commentCountText?: string;
+  viewCount?: number;
+  likeCount?: number;
+  commentCountText?: string;
 }
 
 export interface Map$VideoStatistics {
-    viewCount?: any;
-    likeButton?: any;
-    commentCount?: any;
+  viewCount?: any;
+  likeButton?: any;
+  commentCount?: any;
 }
 
 export default class Resource$VideoStatistics {
+  static parse(data: Map$Video): Schema$VideoStatistics {
+    const map = Resource$VideoStatistics.map(data);
 
-    static parse(data: Map$Video): Schema$VideoStatistics {
-        const map = Resource$VideoStatistics.map(data);
+    let VideoStatistics: Schema$VideoStatistics = {};
+    VideoStatistics["viewCount"] = getNumber(map.viewCount);
+    VideoStatistics["likeCount"] = getNumber(
+      map.likeButton?.likeButtonViewModel.likeButtonViewModel
+        .toggleButtonViewModel.toggleButtonViewModel.defaultButtonViewModel
+        .buttonViewModel.title
+    );
+    VideoStatistics["commentCountText"] = map.commentCount?.simpleText;
 
-        let VideoStatistics: Schema$VideoStatistics = {};
-        VideoStatistics['viewCount'] = getNumber(map.viewCount);
-        VideoStatistics['likeCount'] = getNumber(map.likeButton?.toggleButtonRenderer?.defaultText?.accessibility?.accessibilityData?.label);
-        VideoStatistics['commentCountText'] = map.commentCount?.simpleText;
-        
-        return VideoStatistics;
-    }
+    return VideoStatistics;
+  }
 
-    private static map(data: Map$Video): Map$VideoStatistics {
-        return {
-            viewCount: data.videoDetails?.viewCount,
-            likeButton: data.videoPrimaryInfoRenderer?.videoActions?.menuRenderer?.topLevelButtons?.find((p: any) => p?.segmentedLikeDislikeButtonRenderer)?.segmentedLikeDislikeButtonRenderer?.likeButton,
-            commentCount: data.commentsEntryPoint?.find((p: any) => p?.commentsEntryPointHeaderRenderer)?.commentsEntryPointHeaderRenderer?.commentCount,
-        }
-    }
+  private static map(data: Map$Video): Map$VideoStatistics {
+    const like =
+      data.videoPrimaryInfoRenderer?.videoActions?.menuRenderer?.topLevelButtons?.find(
+        (p: any) => Boolean(p?.segmentedLikeDislikeButtonViewModel)
+      )?.segmentedLikeDislikeButtonViewModel;
+    return {
+      viewCount: data.videoDetails?.viewCount,
+      likeButton:
+        data.videoPrimaryInfoRenderer?.videoActions?.menuRenderer?.topLevelButtons?.find(
+          (p: any) => Boolean(p?.segmentedLikeDislikeButtonViewModel)
+        )?.segmentedLikeDislikeButtonViewModel,
+      commentCount: data.commentsEntryPoint?.find(
+        (p: any) => p?.commentsEntryPointHeaderRenderer
+      )?.commentsEntryPointHeaderRenderer?.commentCount,
+    };
+  }
 }
